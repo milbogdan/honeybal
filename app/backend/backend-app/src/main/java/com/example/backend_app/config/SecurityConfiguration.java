da61d246e151abf86eba521bb5cd86df5a873534
Bogdan <bogdanmilojevic5@gmail.com>
2025-02-12 02:04:22 +0100

    cors fix

--- app/backend/backend-app/src/main/java/com/example/backend_app/config/SecurityConfiguration.java
+++ app/backend/backend-app/src/main/java/com/example/backend_app/config/SecurityConfiguration.java
@@ -7,8 +7,14 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -38,6 +44,10 @@ public class SecurityConfiguration {
        http.authenticationProvider(authenticationProvider);
        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
+
+       //http.cors(cors->cors.disable());
+
        return http.build();
     }
+
 }
