4dd7d135263c77d78996c6e296ed6d5b0bb406e9
Bogdan <bogdanmilojevic5@gmail.com>
2025-02-17 19:33:36 +0100

    fixes

--- app/backend/backend-app/src/main/java/com/example/backend_app/products/repositories/ProductRepository.java
+++ app/backend/backend-app/src/main/java/com/example/backend_app/products/repositories/ProductRepository.java
@@ -14,9 +14,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("SELECT DISTINCT p FROM Product p " +
             "LEFT JOIN p.variations v " +
             "WHERE (:nameSearch IS NULL OR p.name LIKE CONCAT('%', :nameSearch, '%')) " +
-            "AND (:categoryId IS NULL OR p.category.id = :categoryId) " +
-            "AND (:inStock IS NULL OR v.in_stock = :inStock)")
-    Page<Product> findAllWithSearchAndPagination(Pageable pageable, @Param("categoryId") Integer categoryId, @Param("nameSearch") String nameSearch,@Param("inStock") Boolean inStock);
+            "AND (:categoryId IS NULL OR p.category.id = :categoryId) ")
+    Page<Product> findAllWithSearchAndPagination(Pageable pageable, @Param("categoryId") Integer categoryId, @Param("nameSearch") String nameSearch);
 
     @Query("SELECT p FROM Product p WHERE p.name = :name AND p.category = :category AND p.description = :description")
     Product findByNameCategoryAndDescription(String name, ProductCategory category, String description);
