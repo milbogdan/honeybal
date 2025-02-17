f6d857ed66f2ef7f7a341d04796494f9167491d6
Mihajlo <nmihajlon@gmail.com>
2025-02-17 15:28:45 +0100

    sidebar version 2

--- app/frontend/angular/src/app/components/elements/sidenav/sidenav.component.ts
+++ app/frontend/angular/src/app/components/elements/sidenav/sidenav.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { navbarData } from './nav-data';
 
+interface SideNavToggle{
+  screenWidth : number;
+  collapsed : boolean;
+}
+
 @Component({
-  selector: 'app-sidenav',
+  selector: 'sidenav',
   standalone: false,
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
 export class SidenavComponent {
-  collapese : boolean = false;
+  @Output() onToggleSideNav : EventEmitter<SideNavToggle> = new EventEmitter<SideNavToggle>();
+  collapsed : boolean = false;
+  screenWidth = 0;
   navData = navbarData;
+
+  toggleCollapse() : void{
+    this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({
+      collapsed: this.collapsed,
+      screenWidth: this.screenWidth
+    });
+  }
+
+  closeSidenav() : void{
+    this.collapsed = false;
+    this.onToggleSideNav.emit({
+      collapsed: this.collapsed,
+      screenWidth: this.screenWidth
+    });
+  }
 }
--- app/frontend/angular/src/app/pages/admin-page/admin-page.component.ts
+++ app/frontend/angular/src/app/pages/admin-page/admin-page.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface SideNavToggle{
+  screenWidth : number;
+  collapsed : boolean;
+}
+
 @Component({
   selector: 'app-admin-page',
   standalone: false,
@@ -7,5 +12,10 @@ import { Component } from '@angular/core';
   styleUrl: './admin-page.component.css'
 })
 export class AdminPageComponent {
-
+  isSideNavCollapsed = false;
+  screenWidth = 0;
+  onToggleSideNav(data : SideNavToggle) : void{
+    this.screenWidth = data.screenWidth;
+    this.isSideNavCollapsed = data.collapsed;
+  }
 }
