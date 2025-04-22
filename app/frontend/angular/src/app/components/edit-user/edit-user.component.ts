a9e0a542d9b07183bb426ff3d3d8a749decff974
nmihajlon <nmihajlon@gmail.com>
2025-04-23 01:46:11 +0200

    FE: temp commit

--- app/frontend/angular/src/app/components/edit-user/edit-user.component.ts
+++ app/frontend/angular/src/app/components/edit-user/edit-user.component.ts
@@ -31,6 +31,8 @@ export class EditUserComponent {
           firstName: new FormControl(`${this.user?.firstName}`),
           lastName: new FormControl(this.user?.lastName),
           address: new FormControl(this.user?.address),
+          username: new FormControl(this.user?.username),
+          phone: new FormControl(this.user?.phone),
         })
       }
     })
--- app/frontend/angular/src/app/models/user.interface.ts
+++ app/frontend/angular/src/app/models/user.interface.ts
@@ -9,4 +9,5 @@ export interface User {
     role: string;
     updatedAt: string;
     username: string;
+    phone?: number;
 }
