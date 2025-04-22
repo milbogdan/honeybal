76902bb2a1032752b55a4e707e394bc03ea0299b
nmihajlon <nmihajlon@gmail.com>
2025-04-22 14:45:23 +0200

    FE: mock and design for wish list

---
+++ app/frontend/angular/src/app/components/wish-item/wish-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wish-item',
+  imports: [],
+  templateUrl: './wish-item.component.html',
+  styleUrl: './wish-item.component.css'
+})
+export class WishItemComponent {
+
+}
--- app/frontend/angular/src/app/pages/profile-page/profile-page.component.ts
+++ app/frontend/angular/src/app/pages/profile-page/profile-page.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { EditUserComponent } from '../../components/edit-user/edit-user.component';
 import { OrderTableComponent } from '../../components/order-table/order-table.component';
+import { WishItemComponent } from '../../components/wish-item/wish-item.component';
 
 @Component({
   selector: 'app-profile-page',
-  imports: [NavbarComponent, EditUserComponent, OrderTableComponent],
+  imports: [NavbarComponent, EditUserComponent, OrderTableComponent, WishItemComponent],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.css'
 })
